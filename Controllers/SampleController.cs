1f0d6480b8f9a5c31d08e8e35a94c7bc106c22e3
Scoutzknifez <codygaming@4alexanders.com>
2020-01-06 20:01:30 -0800

    Add project files.

---
+++ Controllers/SampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendExperimental.Controllers
+{
+    [ApiController]
+    public class SampleController : ControllerBase {
+        [HttpGet]
+        [Route(Constants.API_HEADER + "ping")]
+        public String getResponse() => "pong";
+
+        [HttpGet]
+        [Route(Constants.API_HEADER + "hello")]
+        public String getHelloWithName(String name) => "Hello " + name;
+    }
+}
---
+++ Utility/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class Constants
+{
+    public const String API_HEADER = "/API/";
+}
